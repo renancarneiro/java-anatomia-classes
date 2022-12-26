9363a98e2f69a3dcc68bd69e6f645a5412a66b8c
Renan Carneiro Costa <renan.carneiro@tre-pa.jus.br>
2022-12-26 15:34:19 -0300

    tipos variaveis

---
+++ src/edu/renan/primeirasemana/TiposVariaveis.java
@@ -0,0 +1,14 @@
+package edu.renan.primeirasemana;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        // tipos variáveis
+        byte idade = 127; // byte vai de -128 a 127
+        short paises = 10000; // até 32767
+        int salario = 800000000; //
+        long galaxias = 8000000001232130000L;
+        float grama  = 2.00123123F;
+        double mmg = 0.3123123123123213;
+
+    }
+}
